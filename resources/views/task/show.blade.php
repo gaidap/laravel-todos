48fa783e8db24c79cde78758aa0c242fffea7dcf
Paul Gaida <pc.gaida@gmail.com>
2023-12-11 21:02:15 +0100

    Add long task description and timestamps display
    The task detail page now includes the long description of the task if it exists. Additionally, timestamps of task creation and last update are also being displayed.

--- resources/views/task/show.blade.php
+++ resources/views/task/show.blade.php
@@ -9,4 +9,15 @@
     <p>
        Description: {{ $task->description }}
     </p>
+    @if($task->long_description)
+        <p>
+           Long Description: {{ $task->long_description }}
+        </p>
+    @endif
+    <p>
+       Created At: {{ $task->created_at }}
+    </p>
+    <p>
+       Updated At: {{ $task->updated_at }}
+    </p>
 </div>
