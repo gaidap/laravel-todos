ebd84e81a4bc234829bfdfd288f25ca244fc14ed
Paul Gaida <paul.gaida@dsmz.de>
2024-01-02 13:30:42 +0100

    Refactor form layout and extract styles to separate file
    The form layout in 'form.blade.php' was refactored to improve usability and readability. The inline styles were removed and extracted to a separate 'styles.blade.php' file to improve code maintainability and reusability. The form fields were re-arranged for better user flow.

--- resources/views/layouts/form.blade.php
+++ resources/views/layouts/form.blade.php
@@ -2,60 +2,52 @@
 
 @section('title', isset($task) ? 'Edit Task' :  'Create Task')
 
-@section('styles')
-    <style>
-        .invalid-feedback {
-            display: block;
-            color: red;
-            font-size: 0.8rem;
-        }
-    </style>
-@endsection
-
 @section('content')
+    <nav class="mb-4">
+        <a class="link" href="{{ isset($task) ? route('tasks.show', ['task' => $task->id]) : route('tasks.index') }}">◄ Go back</a>
+    </nav>
     <form method="POST" action="{{ isset($task) ? route('tasks.store', ['task' => $task->id]) : route('tasks.store') }}">
         @csrf
         @isset($task)
             @method('PUT')
         @endisset
-        <div class="form-group">
+        <div class="mb-4">
             <label for="title">Title</label>
             <input
-                type="text"
-                class="form-control @error('title') is-invalid @enderror"
-                id="title"
-                name="title"
-                value="{{ $task->title ?? old('title') }}"
-                placeholder="Enter title"
+                    @class(['is-invalid' => $errors->has('title')])
+                    id="title"
+                    name="title"
+                    type="text"
+                    value="{{ $task->title ?? old('title') }}"
+                    placeholder="Enter title"
             >
             @error('title')
             <div class="invalid-feedback">{{ $message }}</div>
             @enderror
         </div>
-        <div class="form-group">
+        <div class="mb-4">
             <label for="description">Description</label>
             <textarea
-                class="form-control @error('description') is-invalid @enderror"
-                id="description"
-                name="description"
-                rows="3"
-                placeholder="Enter description"
+                    @class(['is-invalid' => $errors->has('description')])
+                    id="description"
+                    name="description"
+                    rows="3"
+                    placeholder="Enter description"
             >{{ $task->description ?? old('description') }}</textarea>
             @error('description')
             <div class="invalid-feedback">{{ $message }}</div>
             @enderror
         </div>
-        <div class="form-group">
+        <div class="mb-4">
             <label for="long_description">Long Description</label>
             <textarea
-                class="form-control"
-                id="long_description"
-                name="long_description"
-                rows="10"
-                placeholder="Enter long description"
+                    id="long_description"
+                    name="long_description"
+                    rows="10"
+                    placeholder="Enter long description"
             >{{ $task->long_description ??  old('long_description') }}</textarea>
         </div>
-        <button type="submit" class="btn btn-primary">
+        <button type="submit" class="btn">
             @isset($task)
                 Update
             @else
--- resources/views/layouts/styles.blade.php
+++ resources/views/layouts/styles.blade.php
@@ -7,6 +7,22 @@
     .link {
         @apply font-medium text-slate-700 underline decoration-pink-500;
     }
+
+    .invalid-feedback {
+        @apply block text-red-500 text-sm;
+    }
+
+    .is-invalid {
+        @apply border-red-500 !important;
+    }
+
+    label {
+        @apply block mb-2 uppercase text-slate-700;
+    }
+
+    input[type="text"], textarea {
+        @apply block appearance-none w-full px-3 py-2 text-slate-700 border border-slate-300 rounded-md shadow-sm focus:ring-slate-500 focus:border-slate-500 mb-2 leading-tight;
+    }
 </style>
 {{-- blade-formatter-enable --}}
 @yield('styles')
